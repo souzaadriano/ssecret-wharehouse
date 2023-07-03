ec629b1b953b14f51adfdd1f9048a7044eedaad2
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-03 17:28:42 -0300

    feat(database.engine): added methods to intercat to databases more easily

--- src/engines/database/database.engine.ts
+++ src/engines/database/database.engine.ts
@@ -1,4 +1,5 @@
 import { Singleton } from '@/helpers/singleton.decorator';
+import { PreparedQuery } from '@pgtyped/runtime';
 import { Client } from 'pg';
 import { IEngine } from '../engine.contract';
 import { DatabaseConfig } from './database.config';
@@ -12,6 +13,20 @@ export class DatabaseEngine implements IEngine {
     if (this._client) return;
     this._client = new Client(this._configuration);
     await this._client.connect();
+  }
+
+  public async query<INPUT, OUTPUT>(params: INPUT, fn: PreparedQuery<INPUT, OUTPUT>): Promise<OUTPUT[]> {
+    this._isConnected();
+    return await fn.run(params, this._client);
+  }
+
+  public async queryOne<INPUT, OUTPUT>(params: INPUT, fn: PreparedQuery<INPUT, OUTPUT>): Promise<OUTPUT | undefined> {
+    const [output] = await this.query(params, fn);
+    return output;
+  }
+
+  public async execute<INPUT, OUTPUT>(params: INPUT, fn: PreparedQuery<INPUT, OUTPUT>): Promise<void> {
+    await this.query(params, fn);
   }
 
   private _isConnected(): void {
