58ad5982bb01e4dcae7ae2b01c709da2b01c24a7
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-03 17:26:59 -0300

    feat(users table): updated schema, querie and sql files

--- src/engines/database/queries/users.queries.ts
+++ src/engines/database/queries/users.queries.ts
@@ -3,10 +3,13 @@ import { PreparedQuery } from '@pgtyped/runtime';
 
 /** 'CreateUser' parameters type */
 export interface ICreateUserParams {
+  createdAt?: Date | string | null | void;
+  deletedAt?: Date | string | null | void;
   email?: string | null | void;
   hash?: string | null | void;
   id?: string | null | void;
   name?: string | null | void;
+  updatedAt?: Date | string | null | void;
 }
 
 /** 'CreateUser' return type */
@@ -18,15 +21,31 @@ export interface ICreateUserQuery {
   result: ICreateUserResult;
 }
 
-const createUserIR: any = {"usedParamSet":{"id":true,"name":true,"email":true,"hash":true},"params":[{"name":"id","required":false,"transform":{"type":"scalar"},"locs":[{"a":62,"b":64}]},{"name":"name","required":false,"transform":{"type":"scalar"},"locs":[{"a":67,"b":71}]},{"name":"email","required":false,"transform":{"type":"scalar"},"locs":[{"a":74,"b":79}]},{"name":"hash","required":false,"transform":{"type":"scalar"},"locs":[{"a":82,"b":86}]}],"statement":"INSERT INTO\n    \"users\" (id, \"name\", email, hash)\nVALUES\n    (:id, :name, :email, :hash)"};
+const createUserIR: any = {"usedParamSet":{"id":true,"name":true,"email":true,"hash":true,"createdAt":true,"updatedAt":true,"deletedAt":true},"params":[{"name":"id","required":false,"transform":{"type":"scalar"},"locs":[{"a":178,"b":180}]},{"name":"name","required":false,"transform":{"type":"scalar"},"locs":[{"a":191,"b":195}]},{"name":"email","required":false,"transform":{"type":"scalar"},"locs":[{"a":206,"b":211}]},{"name":"hash","required":false,"transform":{"type":"scalar"},"locs":[{"a":222,"b":226}]},{"name":"createdAt","required":false,"transform":{"type":"scalar"},"locs":[{"a":237,"b":246}]},{"name":"updatedAt","required":false,"transform":{"type":"scalar"},"locs":[{"a":257,"b":266}]},{"name":"deletedAt","required":false,"transform":{"type":"scalar"},"locs":[{"a":277,"b":286}]}],"statement":"INSERT INTO\n    \"users\" (\n        \"id\",\n        \"name\",\n        \"email\",\n        \"hash\",\n        \"createdAt\",\n        \"updatedAt\",\n        \"deletedAt\"\n    )\nVALUES\n    (\n        :id,\n        :name,\n        :email,\n        :hash,\n        :createdAt,\n        :updatedAt,\n        :deletedAt\n    )"};
 
 /**
  * Query generated from SQL:
  * ```
  * INSERT INTO
- *     "users" (id, "name", email, hash)
+ *     "users" (
+ *         "id",
+ *         "name",
+ *         "email",
+ *         "hash",
+ *         "createdAt",
+ *         "updatedAt",
+ *         "deletedAt"
+ *     )
  * VALUES
- *     (:id, :name, :email, :hash)
+ *     (
+ *         :id,
+ *         :name,
+ *         :email,
+ *         :hash,
+ *         :createdAt,
+ *         :updatedAt,
+ *         :deletedAt
+ *     )
  * ```
  */
 export const createUser = new PreparedQuery<ICreateUserParams,ICreateUserResult>(createUserIR);
--- src/engines/database/sql/users.sql
+++ src/engines/database/sql/users.sql
@@ -1,5 +1,21 @@
 /* @name createUser */
 INSERT INTO
-    "users" (id, "name", email, hash)
+    "users" (
+        "id",
+        "name",
+        "email",
+        "hash",
+        "createdAt",
+        "updatedAt",
+        "deletedAt"
+    )
 VALUES
-    (:id, :name, :email, :hash);
+    (
+        :id,
+        :name,
+        :email,
+        :hash,
+        :createdAt,
+        :updatedAt,
+        :deletedAt
+    );
