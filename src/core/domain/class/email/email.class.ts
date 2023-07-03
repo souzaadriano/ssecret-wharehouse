b8f03c57084641564b2ce6367c7e364425944a36
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-03 17:48:34 -0300

    feat(email.class): implements a validation for email

--- src/core/domain/class/email/email.class.ts
+++ src/core/domain/class/email/email.class.ts
@@ -1,5 +1,13 @@
+import { InvalidInputException } from '../../exception/shared/invalid-input.exception';
+
 export class Email {
+  private static readonly _validate = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, 'gm');
   constructor(private readonly _value: string) {}
+
+  static create(value: string): Email {
+    if (Email._validate.test(value)) return new Email(value);
+    throw new InvalidInputException(`${value} is a invalid email`, { value });
+  }
 
   get value(): string {
     return this._value;
