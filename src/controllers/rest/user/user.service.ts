f6ef512fd0f40c2672b3c4411013a018d7aa1650
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-04 12:05:02 -0300

    feat(user.service): add create-user to service

--- src/controllers/rest/user/user.service.ts
+++ src/controllers/rest/user/user.service.ts
@@ -1,11 +1,15 @@
+import { CreateUserFactory } from '@/factories/use-cases/create-user.factory';
 import { Injectable } from '@nestjs/common';
+import { CreateUserSchema } from './schemas';
 
 @Injectable()
 export class UserService {
   private readonly _startedAt = new Date();
+  private readonly _createUserUseCase = CreateUserFactory.instance();
 
-  async create() {
-    return {};
+  async create(input: CreateUserSchema) {
+    const { email, id } = await this._createUserUseCase.handle(input);
+    return { email, id };
   }
 
   async list() {
