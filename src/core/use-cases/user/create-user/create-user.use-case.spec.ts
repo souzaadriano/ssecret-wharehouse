c22db5dbf22e57adfca038444ca43f17df7c2d4b
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-04 11:44:19 -0300

    test(create-user.use-case test): test success flow

--- src/core/use-cases/user/create-user/create-user.use-case.spec.ts
+++ src/core/use-cases/user/create-user/create-user.use-case.spec.ts
@@ -1,17 +1,16 @@
-import { GeneratorHandler } from '@/core/adapters/handlers/generator-handler/generator-handler.adapter';
-import { GeneratorHandlerMock } from '@/core/adapters/handlers/generator-handler/generator-handler.mock';
+import { IGeneratorHandler } from '@/core/adapters/handlers/generator-handler/generator-handler.contract';
+import { FAKE_UUID, GeneratorHandlerMock } from '@/core/adapters/handlers/generator-handler/generator-handler.mock';
 import { IHashHandler } from '@/core/adapters/handlers/hash-handler/hash-handler.contract';
+import { HashHandlerMock } from '@/core/adapters/handlers/hash-handler/hash-handler.mock';
 import { IUserRepository } from '@/core/adapters/repositories/user-repository/user-repository.contract';
-import { Uuid } from '@/core/domain/class/uuid/uuid.class';
-import { mock } from 'jest-mock-extended';
+import { UserRepositoryMock } from '@/core/adapters/repositories/user-repository/user-repository.mock';
 import { CreateUserUseCase } from './create-user.use-case';
 
 describe('create-user.use-case', () => {
   let sut: CreateUserUseCase;
-  GeneratorHandlerMock.override('uuid').return(new Uuid('test'));
-  const generatorHandler = GeneratorHandler.mock();
-  const hashHandler: IHashHandler = mock<IHashHandler>();
-  const userRepository: IUserRepository = mock<IUserRepository>();
+  const generatorHandler: IGeneratorHandler = GeneratorHandlerMock.get();
+  const hashHandler: IHashHandler = HashHandlerMock.get();
+  const userRepository: IUserRepository = UserRepositoryMock.get();
 
   beforeAll(() => {
     sut = new CreateUserUseCase({
@@ -28,6 +27,9 @@ describe('create-user.use-case', () => {
       password: 'password1234',
     });
 
-    expect(result.id).toBe('test');
+    expect(result.id).toBe(FAKE_UUID);
+    expect(generatorHandler.uuid).toBeCalledTimes(1);
+    expect(hashHandler.generate).toBeCalledTimes(1);
+    expect(userRepository.save).toBeCalledTimes(1);
   });
 });
