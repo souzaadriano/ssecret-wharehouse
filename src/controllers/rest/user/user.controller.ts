1dc0e1b20138192c3028c4c85c8b0384addb7121
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-04 23:09:59 -0300

    feat(user.controller): update controller to use request-handler

--- src/controllers/rest/user/user.controller.ts
+++ src/controllers/rest/user/user.controller.ts
@@ -1,18 +1,21 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserFactory } from '@/factories/use-cases/create-user.factory';
+import { Body, Controller, Get, Post, Response } from '@nestjs/common';
+import { Response as ExpressResponse } from 'express';
+import { RestRequestHandler } from '../adapters/request-handler/request-handler.service';
 import { CreateUserSchema } from './schemas';
-import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly service: UserService) {}
+  private readonly _createUser = CreateUserFactory.instance();
+
+  constructor(private readonly _requestHandler: RestRequestHandler) {}
 
   @Post()
-  async createUser(@Body() input: CreateUserSchema) {
-    return await this.service.create(input);
+  async createUser(@Body() input: CreateUserSchema, @Response() response: ExpressResponse) {
+    const { status, output } = await this._requestHandler.execute(input, this._createUser);
+    return response.status(status).json(output);
   }
 
   @Get()
-  async listUsers() {
-    return await this.service.list();
-  }
+  async listUsers() {}
 }
