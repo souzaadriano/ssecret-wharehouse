fb60fdad43e7752a5e6605b0069957e6bacc327b
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-05 10:26:34 -0300

    feat(event.engine): implements a engine for event handlers (working in progress)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { Bootstrap } from './engines/bootstrap';
 import { DatabaseEngine } from './engines/database/database.engine';
+import { EventEngine } from './engines/event/event.engine';
 import { NestEngine } from './engines/nest/nest.engine';
 
-const application = Bootstrap.createApp([DatabaseEngine, NestEngine]);
+const application = Bootstrap.createApp([EventEngine, DatabaseEngine, NestEngine]);
 
 application.start().catch(console.error);
